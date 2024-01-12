f9d83449acdb6249cfaade231411c702bdd2631d
Yosief Mekonen <yosephmek16@gmail.com>
2024-01-12 09:45:15 -0800

    Task 1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,21 @@
  */
 int check_num(char *str)
 {
-        /*Declaring variables*/
-        unsigned int count;
+	/*Declaring variables*/
+	unsigned int count;
 
-        count = 0;
-        while (count < strlen(str)) /*count string*/
+	count = 0;
+	while (count < strlen(str)) /*count string*/
 
-        {
-                if (!isdigit(str[count])) /*check if str there are digit*/
-                {
-                        return (0);
-                }
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
 
-                count++;
-        }
-        return (1);
+		count++;
+	}
+	return (1);
 }
 
 /**
@@ -36,30 +36,30 @@ int check_num(char *str)
 int main(int argc, char *argv[])
 
 {
-        /*Declaring variables*/
-        int count;
-        int str_to_int;
-        int sum = 0;
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-        count = 1;
-        while (count < argc) /*Goes through the whole array*/
-        {
-                if (check_num(argv[count]))
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
+	{
+		if (check_num(argv[count]))
 
-                {
-                        str_to_int = atoi(argv[count]); /*ATOI --> converting string to int*/
-                        sum += str_to_int;
-                }
-                /*Condition if one of the number contains symbols that are not digits*/
-                else
-                {
-                        printf("Error\n");
-                        return (1);
-                }
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> converting string to int*/
+			sum += str_to_int;
+		}
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-                count++;
-        }
+		count++;
+	}
 
-        printf("%d\n", sum); /*print sum*/
-        return (0);
+	printf("%d\n", sum); /*print sum*/
+	return (0);
 }
