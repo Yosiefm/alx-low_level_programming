6a7dbdd21fd16e0c65b87bcf26301ca27a69b5f7
Yosief Mekonen <yosephmek16@gmail.com>
2024-02-05 11:22:17 -0800

    header file and others

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t num;
+
+if (h == NULL)
+        return (0);
+for (num = 0; h != NULL; num++)
+{
+	printf("%d\n", h->n);
+	h = h->next;
+}
+return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a readme file for the 0x13. C - More singly linked lists projects
